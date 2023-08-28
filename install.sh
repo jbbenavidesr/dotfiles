664e0331f62a8278aa5349621b04d2613a8cb38e
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2023-08-28 16:07:57 -0500

    Add git config and update names

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -s ${BASEDIR}/zshrc ~/.zshrc
 
 # git
-# ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
