dae8d509d039a3aa4d1e87c64dee7696ace71bac
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2025-05-15 23:59:28 +0200

    [NVIM] Make lazy work completely from plugin directory

--- env/.config/nvim/lua/jbbenavidesr/lazy.lua
+++ env/.config/nvim/lua/jbbenavidesr/lazy.lua
@@ -24,13 +24,4 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require('lazy').setup {
-  spec = {
-    -- import your plugins
-    { import = 'plugins' },
-  },
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { 'habamax' } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-}
+require('lazy').setup("plugins")
