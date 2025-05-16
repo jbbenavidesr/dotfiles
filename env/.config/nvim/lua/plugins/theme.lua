2d8a75a50239f64a73d5c35a3fd59896a0e91b87
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2025-05-16 13:48:56 +0200

    Update theme

---
+++ env/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+-- colorscheme.lua
+--
+-- Set the theme plugin and desired colorscheme
+--
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'catppuccin-mocha'
+  end,
+}
