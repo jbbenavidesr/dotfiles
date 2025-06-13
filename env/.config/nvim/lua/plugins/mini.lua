9ad7c244e378520569c977887a7d7fbe237979b8
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2025-06-13 10:15:06 -0500

    Add line comment toggle functionality

--- env/.config/nvim/lua/plugins/mini.lua
+++ env/.config/nvim/lua/plugins/mini.lua
@@ -26,6 +26,10 @@ return {
     -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
 
+    -- Toggle line comments
+    --
+    require('mini.comment').setup()
+
     -- You can configure sections in the statusline by overriding their
     -- default behavior. For example, here we set the section for
     -- cursor location to LINE:COLUMN
