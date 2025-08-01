07f23776062c3ebe049cb99f342a5c9876985ecd
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2025-08-01 22:13:09 +0100

    💄 Vim, Tmux and Ghostty follow the light/dark settings in Mac

--- env/.config/nvim/lua/plugins/theme.lua
+++ env/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,31 @@
--- colorscheme.lua
+-- theme.lua
 --
 -- Set the theme plugin and desired colorscheme
 --
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
+
+  -- Catppuccin is the theme I'm using everywhere at the moment
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'auto',
+        background = {
+          light = 'latte',
+          dark = 'mocha',
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+
+  -- dark-notify updates neovim's theme with the system settings in Mac
+  {
+    'cormacrelf/dark-notify',
+    config = function()
+      require('dark_notify').run()
+    end,
+  },
 }
